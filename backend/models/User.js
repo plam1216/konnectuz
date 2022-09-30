56a89b04b57af548d8be8f45c4315f869acefbb4
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-09-30 14:16:49 -0400

    postSchema made

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 
+const postSchema = mongoose.Schema({
+    content: String,
+    image: String,
+    comments: [commentSchema],
+    likes: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+}, { 
+    timestamp: true
+});
+
 const userSchema = mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     posts: [postSchema],
     //Image string needs to be a URL to show
     image: String, 
+}, {
+    timestamp: true
 })
 
 const User = mongoose.model("User", userSchema);
