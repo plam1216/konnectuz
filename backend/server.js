60b2a5d31c923e703c926e381d6dcb2de827d399
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-30 13:09:14 -0400

    connected db

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,11 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 require('dotenv').config();
+
+/////////////////////
+//DATABASE CONNECTION
+/////////////////////
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
