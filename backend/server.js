f17645d6ee5be03187b8ca9cbab38a9ba1cd9ac3
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-30 15:26:11 -0400

    changed port

--- backend/server.js
+++ backend/server.js
@@ -42,5 +42,5 @@ app.use('/', (req, res) => {
 ////////////////
 //LISTENING
 ////////////////
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`I can hear you on port ${PORT}`));
