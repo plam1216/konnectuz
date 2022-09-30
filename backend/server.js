8beb64cb32c5dd3cc713743d5903a938f75e1f7c
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-30 14:15:07 -0400

    db connection success/failure

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true,
 });
 
+/////////////////////////////////////
+//DATABASE CONNECTION SUCCESS/FAILURE
+/////////////////////////////////////
+const db = mongoose.connection
+db.on('error', (err) => console.log(err + "is mongo not running?"))
+db.on('connected', () => console.log('mongo connected'))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
 ////////////////
 //ROUTES
