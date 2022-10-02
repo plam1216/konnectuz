eaa3e792a1625801231f8027c3268a903a038580
Drew Anello <drew.anello@gmail.com>
2022-10-02 00:26:55 -0400

    fixes index

--- front end/src/index.js
+++ front end/src/index.js
@@ -2,22 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import Header from './components/Nav';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <App />
-=======
-    <App />
->>>>>>> nav bar is functional
-=======
-      <Header />
->>>>>>> adds nav
     </Router>
   </React.StrictMode>
 );
