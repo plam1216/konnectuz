aaafc95997b95155266dcfc741a887a9e848d85a
Peter Lam <plam1216@gmail.com>
2022-10-01 23:51:36 -0400

    handleSubmit to createUser

--- front end/src/pages/SignUpPage.js
+++ front end/src/pages/SignUpPage.js
@@ -1,12 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 // import Nav from '../components/Nav.js'
-import SignUpForm from '../components/SignUpForm.js'
+// import SignUpForm from '../components/SignUpForm.js'
 
-const SignUpPage = () => {
+// Page Containing SignUp Form
+// Handles Submission by getting 'createUser' prop passed down from SignUpForm
+
+const SignUpPage = (props) => {
+  const [signUp, setSignUp] = useState({
+    username: "",
+    password: "",
+    posts: [],
+    image: ""
+  })
+
+  // handleChanges made to form when user types
+  const handleChange = (event) => {
+    // spread signUp object
+    // reassign [key]:value pairs
+    // console.log(signUp)
+    setSignUp({ ...signUp, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = (event) => {
+    // stop page from refreshing
+    event.preventDefault()
+
+    // createUser method passed down from SignUpForm component
+    props.createUser(signUp)
+
+    // reset signUp
+    setSignUp({
+      username: "",
+      password: "",
+      posts: "",
+      image: ""
+    })
+  }
+
   return (
     <div>
-      {/* <Nav/> */}
-      <SignUpForm/>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          value={signUp.username}
+          name="username"
+          onChange={handleChange}
+          />
+          <label>Password</label>
+        <input
+          type="text"
+          value={signUp.password}
+          name="password"
+          onChange={handleChange}
+          />
+          <label>Profile Picture (URL)</label>
+        <input
+          type="text"
+          value={signUp.image}
+          name="image"
+          onChange={handleChange}
+        />
+
+        <input type="submit" value="Create Profile"></input>
+      </form>
     </div>
   )
 }
