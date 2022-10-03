f0e8f13ac1840d9922793e6c8a429899f2eda7af
Peter Lam <plam1216@gmail.com>
2022-10-03 09:16:03 -0400

    login setup, test after backend setup

--- front end/src/components/LogIn.js
+++ front end/src/components/LogIn.js
@@ -1,22 +1,44 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+// ###########################
+// TEST AFTER BACKEND IS SETUP
+// ###########################
 
 const LogInForm = () => {
-  // handleSubmit() {
-    // event.preventDefault()
-    // check if username and password is valid
-  // }
+
+  // where User data is stored
+  // route should be to wherever API data is
+  const URL = 'http://localhost:4000'
+
+  const [dbUsers, setUser] = useState(null)
+
+  // grab users from MongoDB
+  const getUsers = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setUser(data)
+  }
+
+  // every time the page renders, it will getUsers()
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
-    <div>
-      <h1>Log In</h1>
-      <form>
-        <label>Username</label>
-        <input type="text" name="username"></input>
-        <label>Password</label>
-        <input type="text" name="password"></input>
-      </form>
-    </div>
+      <Route
+        path="/login"
+        render={(rp) => (
+          <LogInPage
+          // pass down users so we can later verify if they exist
+            dbUsers={dbUsers}
+            {...rp}
+          />
+        )}
+      />
   )
+
+
 }
 
 export default LogInForm
