7ebffe0aa8a57352d23374ffdf48846ec811c3aa
Drew Anello <drew.anello@gmail.com>
2022-10-04 15:06:13 -0400

    user page displays ino

--- front end/src/pages/UserPage.js
+++ front end/src/pages/UserPage.js
@@ -1,19 +1,66 @@
 import React from 'react'
 import Header from '../components/Header'
+import { useEffect, useState } from 'react';
 
-// #####################################
-// redirected here after succesful login
-// shows user._id posts
-// #####################################
+// ###############
+// '/feed' route
+// ###############
 
-const UserHome = () => {
-  return (
-    <div>
-      <Header />
-      <h1>This is the user_id's Page</h1>
-      {/* <Posts/> */}
-    </div>
-  )
+// ###########################
+// CURRENTLY USING TEST DATA
+// TEST AFTER BACKEND IS SETUP
+// ###########################
+
+const UserPage = () => {
+
+    const [ user, setUser ] = useState(null);
+
+    const URL = "http://localhost:4000/user/633c758a546c9ecd181ba181/";
+    const getUser = async () => {
+        const repsonse = await fetch(URL);
+        const data = await repsonse.json();
+      // both data and user is showing in console
+        setUser(data);
+    }
+
+    useEffect(() => {getUser()}, []);
+
+
+    let loaded = () => {
+        let allPosts = 
+                <div className="post" key={user._id}>
+                    <h3>{user.username}</h3>
+                    {user.posts.map((post) => {
+                        return (
+                            <div className="postContent" key={post.content}>
+                               {post.image}
+                                {post.content} <br />
+                                
+                                {post.comments.map((comment) => {
+                                    return (
+                                        <div className="comment" key={comment.content}>
+                                            {comment.content}
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+        return (
+            <div>
+            <Header />
+            <h1>User Feed Page</h1>
+            {allPosts}
+        </div>
+        )
+    }
+    let loading = () => {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+    return user ? loaded() : loading();
 }
 
-export default UserHome
+export default UserPage
