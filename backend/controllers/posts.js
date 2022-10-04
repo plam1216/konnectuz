ff55f6659fb067acde50c872060c56ef7adf0505
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-10-04 02:05:44 -0400

    post update route connected to mongo

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -10,6 +10,20 @@ const User = require("../models/user.js");
 //if user session id is equal to post user id
 
 //update(attached to edit route)
+postRouter.put("/:id", async (req, res) => {
+    try {
+        //works so far but doesnt target the post itself
+        res.json(await User.findByIdAndUpdate(req.params.id, 
+            {
+                //sets user.posts value to an object containing all of the keys and values in req.body
+                $set: 
+                {posts: {...req.body}}
+            }, {new: true} 
+        ))
+    } catch (error) {
+        res.send("poopie")
+    }
+})
 
 //create
 postRouter.post("/:id", async (req, res) => {
@@ -19,6 +33,7 @@ postRouter.post("/:id", async (req, res) => {
         res.send("poopie")
     }
 })
+
 //edit (can edit post, button on post)
 
 //show (click comment button and reveals comments pertaining to particular post)
