d9528d1efd9e7613727cf75525d62b6fc1f30509
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-10-05 13:27:45 -0400

    onHover links fixed

--- front end/src/components/Header.js
+++ front end/src/components/Header.js
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <nav>
-            <Link to="/feed">
+            <Link to="/feed" className="nav-link">
                 <img src="https://i.imgur.com/QCHhrSB.png" alt="KonnectuZ logo"  className="logo"/>
             </Link>
-            <Link to="/user/">
+            <Link to="/user/" className="nav-link">
                 <div>Your Page</div>
             </Link>
-            <Link to="/about" className="about">
+            <Link to="/about" className="about nav-link">
                 <div>About uZ</div>
             </Link>
              <span>
-            <Link to="/settings" className="settings">
+            <Link to="/settings" className="settings nav-link">
                 <div>Settings</div>
             </Link>
             </span>
