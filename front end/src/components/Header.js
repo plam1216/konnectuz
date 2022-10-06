22f5d109ac13da34dc7f3e1eaa42216f68f56524
Peter Lam <plam1216@gmail.com>
2022-10-06 10:28:12 -0400

    fixed Header

--- front end/src/components/Header.js
+++ front end/src/components/Header.js
@@ -45,19 +45,31 @@ function Header() {
         )
     } else {
         return (
-            <nav>
-                <Link to="/feed" className="nav-link">
-                    <img src="https://i.imgur.com/QCHhrSB.png" alt="KonnectuZ logo" className="logo" />
-                </Link>
-                <Link to="/user/" className="nav-link">
-                    <div>Your Page</div>
-                </Link>
-                <Link to="/about" className="about nav-link">
-                    <div>About uZ</div>
-                </Link>
-                <Link to="/settings" className="settings nav-link">
-                    <div>Settings</div>
-                </Link>
+            <nav className="navbar" style={{ padding: 0 }}>
+                <div className="container-fluid" style={{ display: "flex" }}>
+                    <Link to="/feed" className="nav-link">
+                        <img src="https://i.imgur.com/QCHhrSB.png" alt="KonnectuZ logo" className="logo" />
+                    </Link>
+                    <ul className="ms-auto text-link">
+                        <li>
+
+                            <Link to="/user/" className="nav-link navbar-brand">
+                                <div>Your Page</div>
+                            </Link>
+                        </li>
+                        <li>
+
+                            <Link to="/about" className="about nav-link navbar-brand">
+                                <div>About uZ</div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/settings" className="settings nav-link navbar-brand">
+                                <div>Settings</div>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         )
     }
